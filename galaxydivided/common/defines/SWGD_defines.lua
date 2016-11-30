0d0d6fb68359d921c1e66a49818b8a976d371533
LastLeviathan <alexander.weindrich@hotmail.com>
2016-11-30 16:46:26 -0500

    Sith Update
    -Fixed hair to remove beards for sith

--- galaxydivided/common/defines/SWGD_defines.lua
+++ galaxydivided/common/defines/SWGD_defines.lua
@@ -6,5 +6,5 @@ NDefines.NGameplay.BORDER_BASE_RADIUS= 6	-- The base radius of the border
 NDefines.NGameplay.BORDER_NEW_COLONY_START = 0.25		-- Newly started colony's borders is modified by this, goes up to 1x with progress
 NDefines.NGameplay.BORDER_POPULATION_MODIFIER = 0.4		-- The border population modifier
 NDefines.NGameplay.CORE_SECTOR_SYSTEM_CAP = 10	-- Too many systems with owned planets in core sector will apply modifier inefficient_planet_management.
-NDefines.NGameplay.TECH_COST_MULT_NUM_COLONIES = 0.2 -- Each Planet adds +X% tech cost
-NDefines.NGameplay.TECH_COST_MULT_NUM_POPS = 0.02 -- Each non-free Pop adds +X% tech cost
+NDefines.NGameplay.TECH_COST_MULT_NUM_COLONIES = 0.15 -- Each Planet adds +X% tech cost
+NDefines.NGameplay.TECH_COST_MULT_NUM_POPS = 0.015 -- Each non-free Pop adds +X% tech cost
