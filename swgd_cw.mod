4d187f79fcce71bb01c72816a86d9633066fe553
LastLeviathan <alexander.weindrich@hotmail.com>
2017-03-10 20:21:14 -0500

    New Project Added
    An early WIP of the current project I'm piecing together from our files.

---
+++ swgd_cw.mod
@@ -0,0 +1,13 @@
+name="!Clone wars SWGD_CW"
+path="mod/swgd_cw"
+tags={
+	"Buildings"
+	"Total Conversion"
+	"Events"
+	"Galaxy Generation"
+	"Star Wars"
+	"Sith"
+	"Empire"
+	"Clone Wars"
+}
+supported_version="1.4.*"
---
+++ swgd_cw/common/defines/SWGD_defines.lua
@@ -0,0 +1,17 @@
+
+NDefines.NGraphics.RANDOM_HEIGHT_MIN = -3
+NDefines.NGraphics.RANDOM_HEIGHT_MAX = 3
+NDefines.NGameplay.START_YEAR = 2200		
+NDefines.NGameplay.BORDER_BASE_RADIUS= 6	-- The base radius of the border
+NDefines.NGameplay.BORDER_NEW_COLONY_START = 0.25		-- Newly started colony's borders is modified by this, goes up to 1x with progress
+NDefines.NGameplay.BORDER_POPULATION_MODIFIER = 0.4		-- The border population modifier
+NDefines.NGameplay.CORE_SECTOR_SYSTEM_CAP = 10	-- Too many systems with owned planets in core sector will apply modifier inefficient_planet_management.
+NDefines.NGameplay.HYPERLANE_WINDUP = 75	-- In micro updates ( 10/day )
+NDefines.NGameplay.HYPERLANE_WINDDOWN = 0	-- In micro updates
+NDefines.NGameplay.HYPERLANE_WINDUP_OUTSIDE_BORDER = 0	-- How much more windup time is there for systems which are 100% away from friendly territory
+NDefines.NPop.EXTERMINATION_MONTHS = 1	-- Number of months it takes to exterminate a population when purging
+
+
+
+
+
